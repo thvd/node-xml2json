6f6ec607047b0962556110ebb938143dc6b1c021
Theodoor van Donge <theodoorvandonge@casenine.com>
2018-08-16 16:38:24 +0200

    Added beautify options for json2xml

--- test/test-json2xml-indent.js
+++ test/test-json2xml-indent.js
@@ -15,9 +15,17 @@ var xml = parser.toXml({
         }
     }
 }, {newLine: '\r', indent: '\t'});
-assert.equal(xml, '<x>\r\t<y>\r\t\t<z>abc</z>\r\t</y>\r\t<a>123</a>\r</x>\r');
+assert.equal(xml, '<x>\r\t<y>\r\t\t<z>abc</z>\r\t</y>\r\t<a>123</a>\r</x>');
 
 
 
-
+var xml1 = parser.toXml({
+    x: {
+        z: {
+            y: {$t: 'abc'},
+            a: {$t: '123'}
+        }
+    }
+}, {newLine: '\r', indent: '\t'});
+assert.equal(xml1, '<x>\r\t<z>\r\t\t<y>abc</y>\r\t\r\t\t<a>123</a>\r\t</z>\r</x>');
 
